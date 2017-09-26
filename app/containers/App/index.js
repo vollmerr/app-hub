acd549401b49a022a7b6df382c9a0f9995c0d817
vollmerr <vollmerr@oregonstate.edu>
2017-09-25 22:35:13 -0700

    remove language

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -21,7 +21,7 @@ export default function App() {
   return (
     <div>
       <Switch>
-        <Route exact path="/" component={HomePage} />
+        <Route exact path={'/'} component={HomePage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
--- app/containers/NotFoundPage/index.js
+++ app/containers/NotFoundPage/index.js
@@ -10,16 +10,11 @@
  */
 
 import React from 'react';
-import { FormattedMessage } from 'react-intl';
-
-import messages from './messages';
 
 export default class NotFound extends React.PureComponent { // eslint-disable-line react/prefer-stateless-function
   render() {
     return (
-      <h1>
-        <FormattedMessage {...messages.header} />
-      </h1>
+      <h1>Page Not Found</h1>
     );
   }
 }
