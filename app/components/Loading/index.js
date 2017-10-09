ec133e2c9d8739bc6c2379909b8878a7b78133a2
vollmerr <Ryan.Vollmer@state.ca.gov>
2017-10-09 11:20:13 -0700

    add error to Loading

--- app/components/Loading/index.js
+++ app/components/Loading/index.js
@@ -1,18 +1,38 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Spinner, SpinnerSize } from 'office-ui-fabric-react/lib/Spinner';
+import styled from 'styled-components';
 
 import Wrapper from './Wrapper';
 
+const ErrorMessage = styled.div`
+  font-size: 24px;
+`;
+
+// https://www.npmjs.com/package/react-loadable
 class Loading extends React.PureComponent {
   render() {
-    const { text } = this.props;
+    const { text, isLoading, timedOut, pastDelay, error } = this.props;
 
-    return (
-      <Wrapper>
-        <Spinner size={SpinnerSize.large} label={text || 'Loading...'} ariaLive={'assertive'} />
-      </Wrapper>
-    );
+    if (isLoading) {
+      if (timedOut) {
+        return <div>Timeoput</div>
+      } else if (pastDelay) {
+        return (
+          <Wrapper>
+            <Spinner size={SpinnerSize.large} label={text || 'Loading...'} ariaLive={'assertive'} />
+          </Wrapper>
+        );
+      }
+    } else if (error) {
+      return (
+        <Wrapper>
+          <div>Sorry, an error has occurred.</div>
+        </Wrapper>
+      );
+    }
+
+    return null;
   }
 }
 
