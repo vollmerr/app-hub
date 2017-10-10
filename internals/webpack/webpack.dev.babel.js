8f2e69d98e9a23b51097efdf1a41b4225194fb63
cwkummer <cwkummer@users.noreply.github.com>
2017-10-10 14:01:10 -0700

    Babel-Polyfill in dev

--- internals/webpack/webpack.dev.babel.js
+++ internals/webpack/webpack.dev.babel.js
@@ -44,6 +44,7 @@ if (dllPlugin) {
 module.exports = require('./webpack.base.babel')({
   // Add hot reloading in development
   entry: [
+    'babel-polyfill',
     'eventsource-polyfill', // Necessary for hot reloading with IE
     'webpack-hot-middleware/client?reload=true',
     path.join(process.cwd(), 'app/app.js'), // Start with js/app.js
