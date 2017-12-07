023bd21b0e00b371297b26a5f035c6aa591177c3
vollmerr <Ryan.Vollmer@state.ca.gov>
2017-12-07 11:49:31 -0800

    fix spaReducer tests

--- app/containers/Spa/reducer.js
+++ app/containers/Spa/reducer.js
@@ -47,7 +47,9 @@ export default handleActions({
     // populate pending and previous 'recipients' and 'acknowledgment' ids
     payload.recipients.forEach((recipient) => {
       const id = recipient[RECIPIENT.ID];
-      if (recipient[RECIPIENT.ACK_DATE]) {
+      const ack = payload.acknowledgments.find((x) => x[ACK.ID] === recipient[RECIPIENT.ACK_ID]);
+      // if the recipient has acknoweldged and acknowledgment is active
+      if (recipient[RECIPIENT.ACK_DATE] || (ack && ack[ACK.STATUS] !== STATUS.ACTIVE)) {
         user.recipientsPreviousIds.push(String(id));
       } else {
         user.recipientsPendingIds.push(String(id));
@@ -109,125 +111,4 @@ export default handleActions({
     // move for all recipients to previous
     // move for user to previous
   },
-
-  // [INIT_DATA_SUCCESS]: (state, action) => state.set('recipient', fromJS(normalizeById(action.payload, RECIPIENT.))),
-  // [DISABLE_ACK_SUCCESS]: (state, action) => state.update('data', (acks) => (
-  //   acks.setIn(
-  //     [acks.findIndex((x) => x.get(ACK.ID) === action.payload[ACK.ID]), ACK.STATUS],
-  //     STATUS.DISABLED,
-  //   )
-  // )),
-  // [GET_RECIPIENTS_SUCCESS]: (state, action) => {
-  //   // just grab id from first entry (as all ack ids will be the same id)
-  //   const id = action.payload[0] && action.payload[0][RECIPIENT.ACK_ID];
-  //   if (id) {
-  //     const recipients = normalizeById(action.payload, RECIPIENT.ACK_ID);
-  //     console.log(recipients);
-  //     // return state.setIn(['recipients', 'byId', id], fromJS(action.payload));
-  //     // return state.setIn(['recipients', 'byId', id], fromJS(action.payload));
-  //   }
-  //   return state;
-  // },
 }, fromJS(initialState));
-
-
-/**
-
-{
-  // [load home page] GET /spa/recipients/{sid} -> list of 'recipient' objects with ackDate/Ids
-  // [load home page] GET /spa/recipients/{sid}/acknowledgments -> list of 'acknowledgments' objects with fileName, etc
-  user: {
-    isCached: true, // determine if should fetch
-    ackPendingIds: [100], // [home page] added when recipientPendingIds generated -> add unique ackId
-    ackPreviousIds: [101], // [home page] added when recipientPreviousIds generated -> add unique ackId
-    recipientPendingIds: [999], // [home page] sid == users sid && !dateAck && ack status == active
-    recipientPreviousIds: [1000], // [home page] sid == users sid && (dateAck || ack status != active)
-  },
-  admin: {
-    isCached: true, // determine if should fetch
-    ackPendingIds: [], // [admin page]
-    ackPreviousIds: [], // [admin page]
-  },
-  // [click on ack - admin page] GET /spa/acknowledgements/{id}/recipients, append to list if not present
-  recipients: {
-    byId: {
-      999: {
-        id: 999,
-        sid: 1,
-        sam,
-        firstName,
-        lastName,
-        email,
-        ackId: 100,
-        ackDate,
-        ...
-      },
-      1000: {
-        id: 1000,
-        sid: 1,
-        sam,
-        firstName,
-        lastName,
-        email,
-        ackId: 101,
-        ackDate,
-        ...
-      },
-      1001: {
-        id: 1001,
-        sid: 999999,
-        sam,
-        firstName,
-        lastName,
-        email,
-        ackId: 101,
-        ackDate,
-        ...
-      },
-    },
-    allIds: [999, 1000, 1001],
-  },
-  // [click on admin page] GET /spa/acknowledgements
-  acknowledgments: {
-    byId:{
-      100: {
-        id: 100
-        title,
-        startDate,
-        endDate,
-        status: 0,
-        creatorGroup: 0,
-        ...
-      },
-      101: {
-        id: 101,
-        title,
-        startDate,
-        endDate,
-        status: 0,
-        creatorGroup: 0,
-        ...
-      },
-    },
-    allIds: [100, 101],
-    activeIds: [100], // [admin page] status == active
-    previousIds: [101], // [admin page] status != active
-  },
-}
-
-// user is not admin
-// loads only acks for self? (merge? if new)
-// display users pending acknowledgments (in list)
-
-// find pending recipients for ack (for list)
-recipients.allIds.filter(x => !recipients.byId[x].ackDate && recipients.byId[x].sid === userSid)
-// find previous recipients for ack (for list)
-recipients.allIds.filter(x => recipients.byId[x].ackDate && recipients.byId[x].sid === userSid)
-
-
-// clicks ack, get details
-
-// user is admin
-// loads all acks
-
- */
--- app/containers/Spa/tests/reducer.test.js
+++ app/containers/Spa/tests/reducer.test.js
@@ -4,76 +4,189 @@ import { fromJS } from 'immutable';
 import spaReducer, { initialState } from '../reducer';
 
 import {
-  INIT_DATA_SUCCESS,
-  DISABLE_ACK_SUCCESS,
-  GET_RECIPIENTS_SUCCESS,
+  GET_USER_DATA_SUCCESS,
+  GET_ADMIN_DATA_SUCCESS,
+  GET_ACK_RECIPIENTS_SUCCESS,
   RECIPIENT,
   ACK,
   STATUS,
 } from '../constants';
 
 const recipients = [
-  { [RECIPIENT.ID]: 1, [RECIPIENT.ACK_ID]: 11, [RECIPIENT.SID]: 111 },
-  { [RECIPIENT.ID]: 2, [RECIPIENT.ACK_ID]: 11, [RECIPIENT.SID]: 222 },
+  { [RECIPIENT.ID]: '0', [RECIPIENT.ACK_ID]: 'a', [RECIPIENT.SID]: '111', [RECIPIENT.ACK_DATE]: null },
+  { [RECIPIENT.ID]: '1', [RECIPIENT.ACK_ID]: 'b', [RECIPIENT.SID]: '222', [RECIPIENT.ACK_DATE]: '12/02/2019' },
+  { [RECIPIENT.ID]: '2', [RECIPIENT.ACK_ID]: 'c', [RECIPIENT.SID]: '111', [RECIPIENT.ACK_DATE]: '10/12/2013' },
+  { [RECIPIENT.ID]: '3', [RECIPIENT.ACK_ID]: 'd', [RECIPIENT.SID]: '222', [RECIPIENT.ACK_DATE]: null },
 ];
 
-const pendingAcks = [
-  { [ACK.ID]: 1, [ACK.TITLE]: 'testPending1', [ACK.STATUS]: STATUS.ACTIVE },
-  { [ACK.ID]: 2, [ACK.TITLE]: 'testPending2', [ACK.STATUS]: STATUS.ACTIVE },
+const acknowledgments = [
+  { [ACK.ID]: 'a', [ACK.TITLE]: 'title a', [ACK.STATUS]: STATUS.ACTIVE },
+  { [ACK.ID]: 'b', [ACK.TITLE]: 'title b', [ACK.STATUS]: STATUS.ACTIVE },
+  { [ACK.ID]: 'c', [ACK.TITLE]: 'title c', [ACK.STATUS]: STATUS.DISABLED },
+  { [ACK.ID]: 'd', [ACK.TITLE]: 'title d', [ACK.STATUS]: STATUS.EXPIRED },
 ];
 
-const previousAcks = [
-  { [ACK.ID]: 3, [ACK.TITLE]: 'testPrevious1', [ACK.STATUS]: STATUS.DISABLED },
-  { [ACK.ID]: 4, [ACK.TITLE]: 'testPrevious2', [ACK.STATUS]: STATUS.EXPIRED },
-];
-
-const data = [
-  ...pendingAcks,
-  ...previousAcks,
-];
+const state = {
+  user: {
+    isCached: false,
+    recipientsPendingIds: ['999'],
+    recipientsPreviousIds: ['123'],
+  },
+  admin: {
+    isCached: false,
+    acksActiveIds: ['999'],
+    acksPreviousIds: ['123'],
+    allIds: ['123', '999'],
+  },
+  recipients: {
+    byId: {
+      abc: { [RECIPIENT.ID]: 'abc', [RECIPIENT.ACK_ID]: '999' },
+    },
+    allIds: ['abc'],
+  },
+  acknowledgments: {
+    byId: {
+      g: { [ACK.ID]: 'g', [ACK.STATUS]: STATUS.ACTIVE },
+    },
+    allIds: ['g'],
+  },
+};
 
 
 describe('spaReducer', () => {
   let expected;
-  let state;
+  let action;
   beforeEach(() => {
     expected = fromJS(initialState);
-    state = fromJS({
-      data,
-      recipients: {},
-    });
   });
 
   it('should return the initial state', () => {
     expect(spaReducer(undefined, {})).toEqual(expected);
   });
 
-  it('should set the data on `INIT_DATA_SUCCESS`', () => {
-    const action = { type: INIT_DATA_SUCCESS, payload: data };
-    const actual = spaReducer(undefined, action).get('data').toJS();
-    expect(actual).toEqual(data);
+  describe('GET_USER_DATA_SUCCESS', () => {
+    beforeEach(() => {
+      const payload = { acknowledgments, recipients };
+      action = { type: GET_USER_DATA_SUCCESS, payload };
+    });
+
+    it('should set the `user` as being cached', () => {
+      expect(spaReducer(undefined, action).getIn(['user', 'isCached'])).toEqual(true);
+    });
+
+    it('should set the `user` with pending recipient ids', () => {
+      expected = fromJS([recipients[0][RECIPIENT.ID]]);
+      expect(spaReducer(undefined, action).getIn(['user', 'recipientsPendingIds'])).toEqual(expected);
+    });
+
+    it('should set the `user` with previous recipient ids', () => {
+      expected = fromJS([recipients[1][RECIPIENT.ID], recipients[2][RECIPIENT.ID], recipients[3][RECIPIENT.ID]]);
+      expect(spaReducer(undefined, action).getIn(['user', 'recipientsPreviousIds'])).toEqual(expected);
+    });
+
+    it('should merge the `recipients` with the existing ones', () => {
+      expected = {
+        byId: {
+          0: recipients[0],
+          1: recipients[1],
+          2: recipients[2],
+          3: recipients[3],
+        },
+        allIds: ['0', '1', '2', '3'],
+      };
+      expect(spaReducer(undefined, action).get('recipients').toJS()).toEqual(expected);
+
+      expected.byId = { ...state.recipients.byId, ...expected.byId };
+      expected.allIds = [...state.recipients.allIds, ...expected.allIds];
+      expect(spaReducer(fromJS(state), action).get('recipients').toJS()).toEqual(expected);
+    });
+
+    it('should merge the `acknowledgments` with the existing ones', () => {
+      expected = {
+        byId: {
+          a: acknowledgments[0],
+          b: acknowledgments[1],
+          c: acknowledgments[2],
+          d: acknowledgments[3],
+        },
+        allIds: ['a', 'b', 'c', 'd'],
+      };
+      expect(spaReducer(undefined, action).get('acknowledgments').toJS()).toEqual(expected);
+
+      expected.byId = { ...state.acknowledgments.byId, ...expected.byId };
+      expected.allIds = [...state.acknowledgments.allIds, ...expected.allIds];
+      expect(spaReducer(fromJS(state), action).get('acknowledgments').toJS()).toEqual(expected);
+    });
   });
 
-  it('should set the acknowledgment as disabled on `DISABLE_ACK_SUCCESS`', () => {
-    const action = { type: DISABLE_ACK_SUCCESS, payload: pendingAcks[0] };
-    const actual = spaReducer(state, action).get('data').toJS();
-    expected = [...data];
-    expected[0][ACK.STATUS] = STATUS.DISABLED;
-    expect(actual).toEqual(expected);
+
+  describe('GET_ADMIN_DATA_SUCCESS', () => {
+    beforeEach(() => {
+      const payload = { acknowledgments };
+      action = { type: GET_ADMIN_DATA_SUCCESS, payload };
+    });
+
+    it('should set the `admin` as being cached', () => {
+      expect(spaReducer(undefined, action).getIn(['admin', 'isCached'])).toEqual(true);
+    });
+
+    it('should set the `admin` with active acknowledgment ids', () => {
+      expected = fromJS([acknowledgments[0][ACK.ID], acknowledgments[1][ACK.ID]]);
+      expect(spaReducer(undefined, action).getIn(['admin', 'acksActiveIds'])).toEqual(expected);
+    });
+
+    it('should set the `admin` with previous acknowledgment ids', () => {
+      expected = fromJS([acknowledgments[2][ACK.ID], acknowledgments[3][ACK.ID]]);
+      expect(spaReducer(undefined, action).getIn(['admin', 'acksPreviousIds'])).toEqual(expected);
+    });
+
+    it('should merge the `acknowledgments` with the existing ones', () => {
+      expected = {
+        byId: {
+          a: acknowledgments[0],
+          b: acknowledgments[1],
+          c: acknowledgments[2],
+          d: acknowledgments[3],
+        },
+        allIds: ['a', 'b', 'c', 'd'],
+      };
+      expect(spaReducer(undefined, action).get('acknowledgments').toJS()).toEqual(expected);
+
+      expected.byId = { ...state.acknowledgments.byId, ...expected.byId };
+      expected.allIds = [...state.acknowledgments.allIds, ...expected.allIds];
+      expect(spaReducer(fromJS(state), action).get('acknowledgments').toJS()).toEqual(expected);
+    });
   });
 
-  it('should set the `recipients` to the `id` of the acknowledgment', () => {
-    const action = { type: GET_RECIPIENTS_SUCCESS, payload: recipients };
-    const actual = spaReducer(state, action).get('recipients').toJS();
-    expected = {
-      [recipients[0][RECIPIENT.ACK_ID]]: recipients,
-    };
-    expect(actual).toEqual(expected);
-  });
 
-  it('should not update the `recipients` if no recipients', () => {
-    const action = { type: GET_RECIPIENTS_SUCCESS, payload: [] };
-    const actual = spaReducer(state, action).get('recipients');
-    expect(actual).toEqual(state.get('recipients'));
+  describe('GET_ACK_RECIPIENTS_SUCCESS', () => {
+    beforeEach(() => {
+      const payload = { recipients, id: recipients[0][RECIPIENT.ACK_ID] };
+      action = { type: GET_ACK_RECIPIENTS_SUCCESS, payload };
+    });
+
+    it('should merge the ack id with the admins `allIds` for caching', () => {
+      expected = [recipients[0][RECIPIENT.ACK_ID]];
+      expect(spaReducer(undefined, action).getIn(['admin', 'allIds']).toJS()).toEqual(expected);
+      expected = [...state.admin.allIds, recipients[0][RECIPIENT.ACK_ID]];
+      expect(spaReducer(fromJS(state), action).getIn(['admin', 'allIds']).toJS()).toEqual(expected);
+    });
+
+    it('should merge the `recipients` with the existing ones', () => {
+      expected = {
+        byId: {
+          0: recipients[0],
+          1: recipients[1],
+          2: recipients[2],
+          3: recipients[3],
+        },
+        allIds: ['0', '1', '2', '3'],
+      };
+      expect(spaReducer(undefined, action).get('recipients').toJS()).toEqual(expected);
+
+      expected.byId = { ...state.recipients.byId, ...expected.byId };
+      expected.allIds = [...state.recipients.allIds, ...expected.allIds];
+      expect(spaReducer(fromJS(state), action).get('recipients').toJS()).toEqual(expected);
+    });
   });
 });
