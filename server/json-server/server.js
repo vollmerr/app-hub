aeb267716b3db9b8eb74b4596ff1c8fd789da712
vollmerr <Ryan.Vollmer@state.ca.gov>
2017-12-11 15:45:15 -0800

    seperate mock api generator, update office-ui, add faker

--- server/json-server/server.js
+++ server/json-server/server.js
@@ -1,84 +1,78 @@
 /* eslint-disable */
-const generate = 0; // change to 1 to generate random data
 const auth = 0; // chagne to 1 to test with authentication
 
-if (generate) {
-  require('./generate');
-} else {
-  const path = require('path');
-  const chalk = require('chalk');
-  const jwt = require('express-jwt');
-  const jsonServer = require('json-server');
-  const server = jsonServer.create();
-  const router = jsonServer.router(path.join(__dirname, '.db.json'));
-  const middlewares = jsonServer.defaults();
-  const redirects = require('./.routes.json');
-  const secret = require('./secret');
+const path = require('path');
+const chalk = require('chalk');
+const jwt = require('express-jwt');
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(__dirname, '.db.json'));
+const middlewares = jsonServer.defaults();
+const redirects = require('./.routes.json');
+const secret = require('./secret');
 
-  const host = 'localhost';
-  const port = 3001;
+const host = 'localhost';
+const port = 3001;
 
-  server.use(middlewares);
+server.use(middlewares);
 
-  // custom routes that require some logic...
-  server.get('/spa/recipients/:id/acknowledgments', (req, res) => {
-    let ackIds = [];
-    let acks = [];
-    // get the list of users acks ids
-    ackIds = router.db
-      .get('spa-recipients')
-      .value()
-      .filter(x => x.SID === req.params.id)
-      .map(x => x.AcknowledgmentID);
-    // get the list of acks based off the users ack ids
-    acks = router.db
-      .get('spa-acknowledgments')
-      .value()
-      .filter(x => ackIds.includes(x.id));
+// custom routes that require some logic...
+server.get('/spa/recipients/:id/acknowledgments', (req, res) => {
+  let ackIds = [];
+  let acks = [];
+  // get the list of users acks ids
+  ackIds = router.db
+    .get('spa-recipients')
+    .value()
+    .filter(x => x.SID === req.params.id)
+    .map(x => x.AcknowledgmentID);
+  // get the list of acks based off the users ack ids
+  acks = router.db
+    .get('spa-acknowledgments')
+    .value()
+    .filter(x => ackIds.includes(x.id));
 
-    res.jsonp(acks);
-  })
+  res.jsonp(acks);
+})
 
-  server.use(jsonServer.rewriter(redirects));
-  server.use(jsonServer.bodyParser);
+server.use(jsonServer.rewriter(redirects));
+server.use(jsonServer.bodyParser);
 
-  // handle patching in correct format
-  // http://jsonpatch.com/
-  // https://tools.ietf.org/html/rfc6902#section-4.3
-  server.use((req, res, next) => {
-    if (req.method === 'PATCH') {
-      let newBody = {};
-      req.body.forEach((x) => {
-        // handle replace ops
-        if (x.op === 'replace') {
-          newBody[x.path.slice(1)] = x.value;
-        }
-      });
-      req.body = newBody;
-    }
-    if (req.method === 'POST') {
-      // set to active status
-      req.body.status = 0;
-    }
-    // Continue to JSON Server router
-    next();
-  })
-
-  if (auth) {
-    server.use(jwt({ secret }));
-    server.use((req, res, next) => {
-      if (req.user) {
-        next(); // continue to JSON Server router
-      } else {
-        res.sendStatus(401);
+// handle patching in correct format
+// http://jsonpatch.com/
+// https://tools.ietf.org/html/rfc6902#section-4.3
+server.use((req, res, next) => {
+  if (req.method === 'PATCH') {
+    let newBody = {};
+    req.body.forEach((x) => {
+      // handle replace ops
+      if (x.op === 'replace') {
+        newBody[x.path.slice(1)] = x.value;
       }
     });
+    req.body = newBody;
   }
+  if (req.method === 'POST') {
+    // set to active status
+    req.body.status = 0;
+  }
+  // Continue to JSON Server router
+  next();
+})
 
-  server.use(router);
-
-  server.listen(port, () => {
-    console.log(`JSON Server at ${chalk.magenta(`http://${host}:${port}`)}`);
+if (auth) {
+  server.use(jwt({ secret }));
+  server.use((req, res, next) => {
+    if (req.user) {
+      next(); // continue to JSON Server router
+    } else {
+      res.sendStatus(401);
+    }
   });
 }
 
+server.use(router);
+
+server.listen(port, () => {
+  console.log(`JSON Server at ${chalk.magenta(`http://${host}:${port}`)}`);
+});
