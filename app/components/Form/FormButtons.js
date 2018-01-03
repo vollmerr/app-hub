d23f7436b9b8a560a865060ada719b540418be1f
vollmerr <Ryan.Vollmer@state.ca.gov>
2018-01-03 11:25:43 -0800

    FormButtons include submit/clear buttons

--- app/components/Form/FormButtons.js
+++ app/components/Form/FormButtons.js
@@ -1,8 +1,58 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
 
 
-const Buttons = styled.div`
+export const Buttons = styled.div`
   padding-top: 15px;
+  display: flex;
+  justify-content: space-between;
 `;
 
-export default Buttons;
+export const Button = styled(DefaultButton)`
+  margin-left: 10px;
+`;
+
+
+export class FormButtons extends React.Component {
+  render() {
+    const { disabled, reset, children } = this.props;
+
+    const submitProps = {
+      disabled,
+      primary: true,
+      text: 'Submit',
+      type: 'submit',
+    };
+
+    const clearProps = {
+      disabled,
+      text: 'Clear',
+      onClick: reset,
+    };
+
+    return (
+      <Buttons>
+        <div>
+          {children}
+        </div>
+        <div>
+          <Button {...submitProps} />
+          <Button {...clearProps} />
+        </div>
+      </Buttons>
+    );
+  }
+}
+
+
+const { bool, node, func } = PropTypes;
+
+FormButtons.propTypes = {
+  disabled: bool,
+  reset: func.isRequired,
+  children: node,
+};
+
+export default FormButtons;
--- app/components/Form/tests/FormButtons.test.js
+++ app/components/Form/tests/FormButtons.test.js
@@ -1,5 +1,43 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { DefaultButton } from 'office-ui-fabric-react/lib/Button';
+
 import { testStyledComponent } from 'utils/testUtils';
 
-import FormButtons from '../FormButtons';
+import FormButtons, { Buttons, Button } from '../FormButtons';
 
-testStyledComponent(FormButtons);
+
+testStyledComponent(Buttons);
+testStyledComponent(Button, DefaultButton);
+
+
+const Child = () => <div>children...</div>;
+const props = {
+  disabled: false,
+  reset: jest.fn(),
+  children: <Child />,
+};
+
+describe('FormButtons', () => {
+  let wrapper;
+  beforeEach(() => {
+    jest.resetAllMocks();
+    wrapper = shallow(<FormButtons {...props} />);
+  });
+
+  it('should render correctly', () => {
+    expect(wrapper).toMatchSnapshot();
+  });
+
+  it('should render Buttons', () => {
+    expect(wrapper.find(Buttons).length).toEqual(1);
+  });
+
+  it('should render two Buttons (submit/clear)', () => {
+    expect(wrapper.find(Button).length).toEqual(2);
+  });
+
+  it('should render children', () => {
+    expect(wrapper.find(Child).length).toEqual(1);
+  });
+});
