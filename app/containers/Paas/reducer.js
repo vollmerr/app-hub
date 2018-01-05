d719e7a92d786d480f6a199ebf98121c392a51d3
vollmerr <Ryan.Vollmer@state.ca.gov>
2018-01-05 15:10:21 -0800

    update paas tests

--- app/containers/Paas/reducer.js
+++ app/containers/Paas/reducer.js
@@ -7,14 +7,9 @@ import * as C from './constants';
 
 
 export const initialState = {
-  authorizations: { // TODO: API!
-    byId: {
-      sid1: { sid: 'sid1', firstName: 'firstName 1', lastName: 'lastName 1', app1: 1, app2: undefined, app3: 0, app4: undefined },
-      sid2: { sid: 'sid2', firstName: 'firstName 2', lastName: 'lastName 2', app1: 1, app2: 1, app3: 1, app4: 1 },
-      sid3: { sid: 'sid3', firstName: 'firstName 3', lastName: 'lastName 3', app1: 0, app2: 0, app3: 0, app4: undefined },
-      sid4: { sid: 'sid4', firstName: 'firstName 4', lastName: 'lastName 4', app1: undefined, app2: undefined, app3: undefined, app4: undefined },
-    },
-    allIds: ['sid1', 'sid2', 'sid3', 'sid4'],
+  authorizations: {
+    byId: {},
+    allIds: [],
   },
   manager: {
     newIds: [],
--- app/containers/Paas/tests/Paas.test.js
+++ app/containers/Paas/tests/Paas.test.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { testMapDispatchToProps } from 'utils/testUtils';
 import AppContainer from 'containers/App-Container';
 
-import { Paas, mapDispatchToProps } from '../Paas';
-import { initDataRequest } from '../actions';
+import { Paas } from '../Paas';
 
 const props = {
   name: 'test name',
   meta: { desc: 'test desc' },
   routes: [{ name: 'route1', key: 'testHome', path: '/testpath' }, { name: 'route2', key: '2', path: '/testpath2' }],
-  onInitDataRequest: jest.fn(),
 };
 
 
@@ -28,13 +25,5 @@ describe('<Paas />', () => {
 
   it('should have render a AppContainer', () => {
     expect(wrapper.find(AppContainer).length).toEqual(1);
-  });
-
-  describe('mapDispatchToProps', () => {
-    const actions = {
-      initDataRequest,
-    };
-
-    testMapDispatchToProps(mapDispatchToProps, actions);
   });
 });
